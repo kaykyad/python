4c0326a85de2cbc42e9a14601f70ee17a77b1895
Lucas <mateus.gomes.dias@escola.pr.gov.br>
2025-05-09 09:41:19 -0300

    exemplo 4ordem das operacoes

---
+++ pythonProject/Capitulo2/exemplo4ordemdasoperacoes.py
@@ -0,0 +1,16 @@
+# A maior prioridade são os parenteses
+# Depois a exponenciacao / potenciacao
+# Depois multiplicacao e divisao
+# soma e subtracao a ordem que vier
+# exemplo
+num1 = 76
+num2 = 5
+num3 = 8
+num4 = 4
+num5 = 10
+num6 = 3
+num7 = 2
+
+total = num1 - (num2 + num3 / num4) * (num5 - num6 * num7)
+
+print(total)
