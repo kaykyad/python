83c4a4ce581b1bb3700fc48d633485b6aaef4595
Lucas <mateus.gomes.dias@escola.pr.gov.br>
2025-05-09 11:43:31 -0300

    exercicio 1

---
+++ pythonProject/Capitulo2/exercicio 1.py
@@ -0,0 +1,15 @@
+# Exercicio 1
+num1 = 4
+num2 = 3
+num3 = 3.14
+num4 = 5
+num5 = 3
+total = (num1 / num2) * num3 * num4 ** num5
+print(total)
+
+ # Correção
+ #priemiroTermo = 4 / 3
+ #pi = 3.14
+ #raio = 5
+
+ #total = primeiroTermo * pi * raio
