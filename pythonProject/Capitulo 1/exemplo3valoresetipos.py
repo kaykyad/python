7fac30903a677b50dd3b45b031a975d53851062d
Lucas <mateus.gomes.dias@escola.pr.gov.br>
2025-05-09 10:56:08 -0300

    exemplos do capitulo 1

---
+++ pythonProject/Capitulo 1/exemplo3valoresetipos.py
@@ -0,0 +1,20 @@
+# Tipos de dados
+# inteiros 12345 ... positivos e negativos
+# ponto flutuante 1.0, 1.3, 5.153269 ... positivos e negativos
+# Caracteres que sao apenas um simbolo
+# strings que sao conjuntoss de caracteres
+
+# Exemplo de uma variavel com numero interio
+num1 = 7
+
+# exemplo de uma variavel com numero de ponto flutuante
+num2 = 3.1457
+
+# exemplo de uma variavel com um caracter
+char1 = 'a'
+
+# exemplo de uma variavel com uma string
+string1 = 'Jose'
+
+# comando para descobrir qual o tipo de variavel
+print(type(num2))
