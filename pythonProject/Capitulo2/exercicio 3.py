6b3e7454b36afed9e9bd3d7a6fe1a5e4b887a4e2
isadora.fiori <Andrey.schmitz@escola.pr.gov.br>
2025-05-16 12:26:19 -0300

    exercicio 3

--- pythonProject/Capitulo2/exercicio 3.py
+++ pythonProject/Capitulo2/exercicio 3.py
@@ -1,17 +1,44 @@
 print ("tempo do primeiro trecho")
-minutoPrimeiro = 8
+minutoPrimeiroTrecho = 8 * 60
 segundoPrimeiroTempo = 15
-totalTempoPrimeiroTrecho = (8 * 60) + 15
-print(" Tempo do primeiro trecho em segundos: ", totalTempoPrimeiroTrecho)
 
 print("Tempo do segundo trecho: 7min e 12seg")
-minutoSegundoTrecho = 7
-segundoSegundotrecho = 12
-totalTempoSegundoTrecho = (7 * 60) + 12
-print("Tempo do segundo trecho em segundos: ", totalTempoSegundoTrecho)
+minutoSegundoTrecho = (7 * 3) * 60
+segundoSegundotrecho = 12 * 3
+
 
 print ("tempo do terceiro trecho: 8 min e 15 s")
-minutoTerceiroTrecho = 8
+minutoTerceiroTrecho = 8 * 60
 segundoTerceiroTempo = 15
-totalTempoTerceiroTrecho = (8 * 60) + 15
-print(" Tempo do terceiro trecho em segundos: ", totalTempoTerceiroTrecho)
+#print(" Tempo do terceiro trecho em segundos: ", totalTempoTerceiroTrecho)
+# soma o total de minumetros e converte todos os minutos em segundos
+
+
+totalTempoTodosTrechosMinutos = (minutoPrimeiroTrecho + minutoSegundoTrecho + minutoTerceiroTrecho) / 60
+
+
+# soma valor total dos segundos
+totalTempoTodosTrechosSegundos = (segundoPrimeiroTempo + segundoSegundotrecho + segundoTerceiroTempo)
+
+
+restantesMinutos = int(totalTempoTodosTrechosSegundos / 60)
+restantesSegundos = totalTempoTodosTrechosSegundos % 60
+
+
+totalMinutos = totalTempoTodosTrechosMinutos + restantesMinutos
+totalSegundos = restantesSegundos
+
+
+print("soma de tempo de todos os trechos: ", totalMinutos, "Minutos")
+print("Soma de tempo de todos os trechos: ", totalSegundos, "segundos")
+
+horaInicialSegundos  = (6 * 60) * 60
+minutoInicialSegundos = 52 * 60
+horarioInicialSegundos = horaInicialSegundos + minutoInicialSegundos
+
+tempoTrechoMinutosSegundos = totalMinutos * 60
+
+horaChegada = int(((horarioInicialSegundos + tempoTrechoMinutosSegundos) / 60) / 60)
+minutoChegada = int(((minutoInicialSegundos + tempoTrechoMinutosSegundos) / 60) % 60)
+
+print(" O tempo de chegada foi de " , horaChegada, ":", minutoChegada, restantesSegundos)
