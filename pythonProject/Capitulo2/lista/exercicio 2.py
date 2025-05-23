0ac036cdc3a16e9aad82bddb9b1a4046697f513d
Lucas <silva.caua27@escola.pr.gov.br>
2025-05-23 10:58:18 -0300

    exercicio 2

--- pythonProject/Capitulo2/lista/exercicio 2.py
+++ pythonProject/Capitulo2/lista/exercicio 2.py
@@ -1,2 +1,15 @@
-numero = input('Digite um numero de 5 digitos')
+numero = int(input("Digite um número de 5 dígitos: "))
 
+
+numero1 = (numero // 10000) % 10
+numero2 = (numero // 1000) % 10
+numero3 = (numero // 100) % 10
+numero4 = (numero // 10) % 10
+numero5 = numero % 10
+
+
+print("  ", numero1)
+print("  ", numero2)
+print("  ", numero3)
+print("  ", numero4)
+print("  ", numero5)
